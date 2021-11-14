619532216e847aea6116e2a9d9cd22d48b16464e
Ombre2 <johnylegenie@gmail.com>
2021-11-14 11:38:32 +1100

    use session

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 var path = require('path')
-var session = require('express-session')
+// var session = require('express-session')
 
 
-//
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 3000 }
-}))
+// //
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { maxAge: 3000 }
+// }))
 
 //Use engine Pug
 app.set('views', './view')
@@ -27,13 +27,13 @@ app.use((req, res, next) => {
    res.render('404', { title: '404', message: '404 not found' });
 })
 
-app.use(function (req, res, next) {
-    if (req.session.views) {
-        req.session.views++
-    } else {
-        req.session.views = 1
-    }
-})
+// app.use(function (req, res, next) {
+//     if (req.session.views) {
+//         req.session.views++
+//     } else {
+//         req.session.views = 1
+//     }
+// })
 
 module.exports = app;
    
